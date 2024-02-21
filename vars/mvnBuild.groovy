5bfa94f960a2e075e2370b3efb9d1f98ec5d1845
Ritesh kumar <50689363+riteshkumar036@users.noreply.github.com>
2024-02-21 21:33:51 +0530

    Update mvnBuild.groovy

--- vars/mvnBuild.groovy
+++ vars/mvnBuild.groovy
@@ -1,3 +1,19 @@
-def call(){
-    sh 'mvn clean install'
+// vars/customMap.groovy
+
+def call(Map config) {
+    // Extract parameters
+    def directory = config.directory ?: error('Directory is required')
+    def mavenGoal = config.mavenGoal ?: 'clean install'
+
+    // Define closure to execute within the directory context
+    def stepsClosure = {
+        // Change directory to the specified location
+        dir(directory) {
+            // Run Maven clean install
+            sh "mvn ${mavenGoal}"
+        }
+    }
+
+    // Execute the closure within the pipeline
+    steps(stepsClosure)
 }
