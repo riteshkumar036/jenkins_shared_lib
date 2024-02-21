c39945fed8531d43141b32b380b2413abfad5ff1
Ritesh kumar <50689363+riteshkumar036@users.noreply.github.com>
2024-02-21 23:04:49 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -10,14 +10,14 @@
 //     sh "docker image push ${hubUser}/${project}:latest"   
 // }
 
-
+/*
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
      aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
-}
+}*/
 
 def call(Map config){
     def imageName = config.imageName ?: error('imageName is required')
