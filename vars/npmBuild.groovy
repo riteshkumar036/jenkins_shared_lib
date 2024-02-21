f6d70a56bc1c0efd81f5b7dc145063ac6c3b7eb5
Ritesh kumar <50689363+riteshkumar036@users.noreply.github.com>
2024-02-21 23:42:58 +0530

    Update npmBuild.groovy

--- vars/npmBuild.groovy
+++ vars/npmBuild.groovy
@@ -1,1 +1,8 @@
-
+def call(Map config) {
+    def directory = config.directory ?: error('Directory is required')
+    sh """
+         cd ${directory}
+         npm i
+         npm run build
+        """
+}
