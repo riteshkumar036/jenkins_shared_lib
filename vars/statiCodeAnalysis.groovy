0a293ed79fd4b97eca7adce491c6c4ec1d796fb1
Ritesh kumar <50689363+riteshkumar036@users.noreply.github.com>
2024-02-22 10:04:35 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -4,3 +4,8 @@ def call(credentialsId){
          sh 'mvn clean package sonar:sonar'
     }
 }
+
+/* mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=k8-project \
+  -Dsonar.host.url=http://13.233.141.61:9000 \
+  -Dsonar.login=sqp_8b8c7330bcd7ad296c7d8a4db8bf993f90543625 */
